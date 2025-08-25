0deeb4a36a3db70902fff8932a3e0e3a70ebfa28
sukesh-ak <sukesh@live.com>
2025-08-26 00:07:36 +0530

    update vcpkg and gitignore

--- server/build.sh
+++ server/build.sh
@@ -1,2 +1,6 @@
-cmake . -B build -DCMAKE_TOOLCHAIN_FILE=/home/sukeshak/HFAT-v2/vcpkg/scripts/buildsystems/vcpkg.cmake
+# git clone https://github.com/microsoft/vcpkg.git
+# cd vcpkg
+# ./bootstrap-vcpkg.sh
+
+cmake . -B build -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build build/
